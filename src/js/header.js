ab3fecbed22b94a46ec77dcf946dfe9caaa78edc
Harrisong <118435010+Harrisong-Gutierrez@users.noreply.github.com>
2023-11-14 15:24:00 -0600

    Feature/about contact page (#11)
    * creating the card
    
    * creating testimonios section
    
    * creating sliders
    
    * creating the components in html
    
    * doing correct import of the header
    
    * configured header
    
    * banner already done
    
    * aboutus section aldready done
    
    * ordering my html
    
    * testimonials section already done
    
    * resolving conflicts in my code
    
    * resolving conflicts in my code
    
    ---------
    
    Co-authored-by: harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>

---
+++ src/js/header.js
@@ -0,0 +1,11 @@
+function loadHeader() {
+  fetch("/src/components/header.html")
+    .then((response) => response.text())
+    .then(
+      (text) => (document.getElementById("common-header").innerHTML = text)
+    );
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  loadHeader();
+});
