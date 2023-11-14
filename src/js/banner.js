acb0ae127bc23544d338715d7475e12d75c04b1e
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2023-11-14 09:08:07 -0600

    banner already done

---
+++ src/js/banner.js
@@ -0,0 +1,11 @@
+const loadBanner = () => {
+  fetch("/src/components/banner.html")
+    .then((response) => response.text())
+    .then(
+      (text) => (document.getElementById("common-banner").innerHTML = text)
+    );
+};
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  loadBanner();
+});
--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,11 @@
-function loadHeader () {
-    fetch('/src/views/header.html')
-    .then(response => response.text())
-    .then(text => document.getElementById('common-header').innerHTML = text)
+function loadHeader() {
+  fetch("/src/components/header.html")
+    .then((response) => response.text())
+    .then(
+      (text) => (document.getElementById("common-header").innerHTML = text)
+    );
 }
 
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    loadHeader();
+document.addEventListener("DOMContentLoaded", function (event) {
+  loadHeader();
 });
